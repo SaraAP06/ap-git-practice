96da02fae1f40b6b52c5c7b4462e5660a4ad7ab2
SaraAP06 <sara.ap4438@gmail.com>
2026-02-11 19:28:44 +0330

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,59 +1,9 @@
-#include "UserManager.h"
-#include <QFile>
-#include <QTextStream>
+QFile file("reservations.txt");
+file.open(QIODevice::Append | QIODevice::Text);
+QTextStream out(&file);
 
-bool UserManager::registerUser(QString username, QString password)
-{
-    QFile file("users.txt");
+out << Session::currentUser << "|"
+    << carId << "|"
+    << date << "|pending\n";
 
-    if(!file.open(QIODevice::ReadWrite | QIODevice::Text))
-        return false;
-
-    QTextStream in(&file);
-
-    // check if user exists
-    while(!in.atEnd())
-    {
-        QString line = in.readLine();
-        QStringList parts = line.split("|");
-
-        if(parts[0] == username)
-        {
-            file.close();
-            return false; // user exists
-        }
-    }
-
-    QTextStream out(&file);
-    out << username << "|" << password << "\n";
-
-    file.close();
-    return true;
-}
-
-bool UserManager::loginUser(QString username, QString password)
-{
-    QFile file("users.txt");
-
-    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
-        return false;
-
-    QTextStream in(&file);
-
-    while(!in.atEnd())
-    {
-        QString line = in.readLine();
-        QStringList parts = line.split("|");
-
-        if(parts.size() >= 2 &&
-           parts[0] == username &&
-           parts[1] == password)
-        {
-            file.close();
-            return true;
-        }
-    }
-
-    file.close();
-    return false;
-}
+file.close();
